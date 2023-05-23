6307ee86779c51e2dee260ca88eb3f34c51b9277
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-05-23 14:46:41 -0400

    integracion de java spring boot

---
+++ src/test/java/dci/isoft/evaluacion3/Evaluacion3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dci.isoft.evaluacion3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
