ed0932e3fc5db4f803c2004290e771e077befe74
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-05-23 23:18:37 -0400

    funcion para mostrar mas popular y arreglos en la rama

--- evaluacion3/src/main/java/dci/isoft/test3/controlador/ControllerCSV.java
+++ evaluacion3/src/main/java/dci/isoft/test3/controlador/ControllerCSV.java
@@ -2,6 +2,8 @@ package dci.isoft.test3.controlador;
 
 import dci.isoft.test3.Model.Users;
 import dci.isoft.test3.Services.CSVService;
+import dci.isoft.test3.servicio.ServicioData1;
+import dci.isoft.test3.modelo.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +20,26 @@ public class ControllerCSV {
     @Autowired
     CSVService csvService;
     ArrayList<Users> usuarios = new ArrayList<>();
+    ArrayList<Usuario> usuarios2 = new ArrayList<>();
     @GetMapping("/listar")
     public List<Users> mostrarUsuarios() throws IOException {
-        return csvService.listar(usuarios);
+        this.usuarios=csvService.listar(usuarios);
+        return this.usuarios;
     }
     @GetMapping("/ultimosDiez")
     public List<Users> ultimosDiez() throws IOException {
         return csvService.ultimosDiezUsuariosConectadosRecientemente(usuarios);
     }
+    @GetMapping("/seguidos")
+    public List<Users> seguidos() throws IOException {
+        return csvService.usuarioConMasSeguidores(usuarios);
+    }
+
+    /*@GetMapping("/inactivoMayorSeguido")
+    public List<Users> inactivoMayorSeguido() throws IOException {
+        return csvService.usuarioInactivoConMasSeguidores(this.usuarios);
+    }
+
+     */
+
 }
